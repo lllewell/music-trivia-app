aa9f0b3c496d8817201af2456ca9f5b1f9b17aeb
Alien <lmllewellyn@gmail.com>
2024-08-08 20:57:20 -0400

    added Game and User models

---
+++ models/Game.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Game extends Model {}
+
+
+Game.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        question_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'question',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'game',
+    }
+);
+
+module.exports = Game;
