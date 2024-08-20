7e1622ae59abe30e8d0239ce269b214a81ac5c6d
Alien <lmllewellyn@gmail.com>
2024-08-20 18:39:10 -0400

    fixed case error for Render

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const userRoutes = require('./UserRoutes');
 const questionRoutes = require('./questionRoutes');
 
-router.use('/users', userRoutes);
+router.use('/Users', userRoutes);
 router.use('/questions', questionRoutes);
 
 module.exports = router;
