a143ab2420937a7827c7bb7b4629d12c3cc2390b
Gee <g.pleasant@lafilm.edu>
2024-08-25 10:51:38 -0500

    updated tone.js and created handleStart function

--- public/js/tone.js
+++ public/js/tone.js
@@ -2,30 +2,19 @@ const player = new Tone.Player({
   url: "/audio/Jeopardy-theme-song.mp3",
   autostart: false,
   loop: true,
-  fadeOut: 3
+  fadeOut: 3,
 }).toDestination();
 
-
-document.getElementById("sound").addEventListener("click", () => {
+const handleStart = () => {
   Tone.start()
     .then(() => {
-      // const vol = new Tone.Volume(-1000).toDestination();
       player.start();
       player.volume.value = -20;
     })
     .catch((error) => {
       console.error("Error starting Tone.js:", error);
     });
-});
+};
 
-document.getElementById("restart").addEventListener("click", () => {
-  Tone.start()
-    .then(() => {
-      player.start();
-      player.volume.value = -20;
-      // const vol = new Tone.Volume(-12).toDestination();
-    })
-    .catch((error) => {
-      console.error("Error starting Tone.js:", error);
-    });
-});
+document.getElementById("sound").addEventListener("click", handleStart);
+document.getElementById("restart").addEventListener("click", handleStart);
