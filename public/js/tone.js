94244b9f871b3ce13e7c0761adc0d935d839a2fe
Gee <g.pleasant@lafilm.edu>
2024-08-25 10:41:36 -0500

    updated volume on tone.js so its lower in db

--- public/js/tone.js
+++ public/js/tone.js
@@ -1,15 +1,17 @@
 const player = new Tone.Player({
   url: "/audio/Jeopardy-theme-song.mp3",
   autostart: false,
+  loop: true,
+  fadeOut: 3
 }).toDestination();
+
 
 document.getElementById("sound").addEventListener("click", () => {
   Tone.start()
     .then(() => {
+      // const vol = new Tone.Volume(-1000).toDestination();
       player.start();
-      player.fadeOut = 3;
-      player.loop= true;
-
+      player.volume.value = -20;
     })
     .catch((error) => {
       console.error("Error starting Tone.js:", error);
@@ -20,8 +22,8 @@ document.getElementById("restart").addEventListener("click", () => {
   Tone.start()
     .then(() => {
       player.start();
-      player.fadeOut = 3;
-      player.loop= true;
+      player.volume.value = -20;
+      // const vol = new Tone.Volume(-12).toDestination();
     })
     .catch((error) => {
       console.error("Error starting Tone.js:", error);
